1cb1058d6c1c060f3d8872eb1cd15fb9e8c1c66d
DevonNutting <devon@hiddengeniusproject.org>
2024-04-23 10:16:15 -0700

    Update

---
+++ Car Crashers/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody _rb;
+    [SerializeField] private float _forwardForce = 2000f;
+    [SerializeField] private float _sidewaysForce = 500f;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate() 
+    {
+        HandleMovement();     
+    }
+
+    private void HandleMovement()
+    {
+        _rb.AddForce(0, 0, _forwardForce * Time.deltaTime); // Move the car forward
+
+        if (Input.GetAxis("Horizontal") > 0) _rb.AddForce(_sidewaysForce * Time.deltaTime, 0, 0); // Move the player right
+        if (Input.GetAxis("Horizontal") < 0) _rb.AddForce(-_sidewaysForce * Time.deltaTime, 0, 0); // Move the player left
+
+    }
+}
