8a9ad69737d880e95535db8215fe976eb3773aeb
DevonNutting <devon@hiddengeniusproject.org>
2024-05-07 19:41:37 -0700

    R.A.B Art Overhaul
    Completed Lesson #8 of the Roll-A-Ball Project:
    
    * Imported the Ghost LowPoly unity package
    * Imported the Pirate Coin unity package
    * Imported Low Poly Dungeon unity asset pack
    * Replaced the player capsule mesh with a ghost
    * replaced the pickup cube mesh with a pirate coin
    * Replaced primitive walls with new art models

--- Roll-A-Ball Summer/Assets/Scripts/Pickup.cs
+++ Roll-A-Ball Summer/Assets/Scripts/Pickup.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    private bool pickedUp = false; // A boolean to track whether or not this pickup has been collected or not
     private void OnTriggerEnter(Collider other) // OnTriggerEnter is a function that is called when a collider marked as a trigger collides with this object
-    {
+    {   
+        if (pickedUp) return; // We do not want to do anything if this pickup is already collected by the player
+
         // If The object this pickup was just touched by was the player: destory this pickup object
-        if (other.gameObject.CompareTag("Player")) Destroy(this.gameObject);
-        GameManager.Instance.UpdateScore(1); // Call the 'UpdateScore()' function from the GameManager script
+        if (other.gameObject.CompareTag("Player")) 
+        {
+            pickedUp = true;
+            Destroy(this.gameObject);
+            GameManager.Instance.UpdateScore(1); // Call the 'UpdateScore()' function from the GameManager script
+            Debug.Log("Pickup");
+        }
     }
 }
